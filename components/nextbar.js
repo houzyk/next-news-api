16f990b1551209fe25bc6d945c2f08fd03a0d0fd
houzairkoussa <houzairmk@icloud.com>
2022-01-04 14:49:30 +0400

    ADD conditional rendering to next bar

--- components/nextbar.js
+++ components/nextbar.js
@@ -7,19 +7,45 @@ import styles from "../styles/Nextbar.module.css";
 export function NextBar ({pageNumber}) {
   const router = useRouter();
 
-  return (
-    <div className={styles.nextbar}>
-      <div
-        onClick={() => router.push('/')}>
-        Home
+  if (pageNumber <= 1) {
+    return (
+      <div className={styles.nextbar}>
+        <div>
+          {pageNumber}
+        </div>
+        <div
+          onClick={() => router.push(`/feed/${pageNumber + 1}`)}>
+          Next
+        </div>
       </div>
-      <div>
-        {pageNumber}
+    );
+  } else if (pageNumber >= 5) {
+    return (
+      <div className={styles.nextbar}>
+        <div
+          onClick={() => router.push(`/feed/${pageNumber - 1}`)}>
+          Previous
+        </div>
+        <div>
+          {pageNumber}
+        </div>
       </div>
-      <div
-        onClick={() => router.push('/about')}>
-        About
+    );
+  } else {
+    return (
+      <div className={styles.nextbar}>
+        <div
+          onClick={() => router.push(`/feed/${pageNumber - 1}`)}>
+          Previous
+        </div>
+        <div>
+          {pageNumber}
+        </div>
+        <div
+          onClick={() => router.push(`/feed/${pageNumber + 1}`)}>
+          Next
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
