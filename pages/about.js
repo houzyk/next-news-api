0b4435ccd93436276004b19cf47c37d222474c9f
houzairkoussa <houzairmk@icloud.com>
2022-01-04 10:59:05 +0400

    ADD styles for about page

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,30 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable @next/next/no-img-element */
+
+// css
+import styles from "../styles/About.module.css";
+
 export function About ({ me }) {
   // returns a page component with profile details
   return (
     <div className="container">
       <h1>{me.name}</h1>
       <p>
-        <a href={me.html_url} target="_blank">
+        <a
+          href={me.html_url}
+          target="_blank"
+        >
           {me.login}
         </a>
       </p>
       <p>
         {me.company}
       </p>
-      <img src={me.avatar_url} alt="profile photo"/>
+      <img
+        src={me.avatar_url}
+        alt="profile photo"
+        className={styles.photo}
+      />
     </div>
   );
 }
