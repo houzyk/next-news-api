06e8f8659f3caa1fd264f8fb592a85f2229033a7
houzairkoussa <houzairmk@icloud.com>
2022-01-04 13:35:33 +0400

    ADD head to about page

--- pages/about.js
+++ pages/about.js
@@ -1,31 +1,39 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable @next/next/no-img-element */
 
+// next hooks
+import Head from 'next/head'
+
 // css
 import styles from "../styles/About.module.css";
 
 export function About ({ me }) {
   // returns a page component with profile details
   return (
-    <div className="container">
-      <h1>{me.name}</h1>
-      <p>
-        <a
-          href={me.html_url}
-          target="_blank"
-        >
-          {me.login}
-        </a>
-      </p>
-      <p>
-        {me.company}
-      </p>
-      <img
-        src={me.avatar_url}
-        alt="profile photo"
-        className={styles.photo}
-      />
-    </div>
+    <>
+      <Head>
+        <title>About - Next News Api</title>
+      </Head>
+      <div className="container">
+        <h1>{me.name}</h1>
+        <p>
+          <a
+            href={me.html_url}
+            target="_blank"
+          >
+            {me.login}
+          </a>
+        </p>
+        <p>
+          {me.company}
+        </p>
+        <img
+          src={me.avatar_url}
+          alt="profile photo"
+          className={styles.photo}
+        />
+      </div>
+    </>
   );
 }
 
