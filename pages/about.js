12378618d7750246c16bba69e89682e8f6c41d5a
houzairkoussa <houzairmk@icloud.com>
2022-01-04 10:53:45 +0400

    ADD about page and fetched gh api data

---
+++ pages/about.js
@@ -0,0 +1,35 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable @next/next/no-img-element */
+export function About ({ me }) {
+  // returns a page component with profile details
+  return (
+    <div className="container">
+      <h1>{me.name}</h1>
+      <p>
+        <a href={me.html_url} target="_blank">
+          {me.login}
+        </a>
+      </p>
+      <p>
+        {me.company}
+      </p>
+      <img src={me.avatar_url} alt="profile photo"/>
+    </div>
+  );
+}
+
+// gets profile data to render server-side
+export async function getServerSideProps (context) {
+  // fetches profile data from github api and parses into json
+  const apiResp = await fetch("https://api.github.com/users/houzyk");
+  const me = await apiResp.json();
+
+  // returns a props object to the About component containing fetched json data
+  return {
+    props: {
+      me
+    }
+  };
+}
+
+export default About;
