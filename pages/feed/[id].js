334404e097da1b162029fbedb481b9204ed15561
houzairkoussa <houzairmk@icloud.com>
2022-01-04 14:27:15 +0400

    ADD data fetching from news api

--- pages/feed/[id].js
+++ pages/feed/[id].js
@@ -19,9 +19,22 @@ export async function getServerSideProps(context) {
     };
   }
 
+  // fetches news data from News API
+  const url = `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`;
+  const apiResponse = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEWS_KEY}`
+    }
+  });
+
+  // parses data into json
+  const data = await apiResponse.json();
+
   return {
     props: {
-      // todo
+      // converts number string into num to allow for better handling in component
+      pageNumber: Number.parseInt(pageNumber),
+      articles: data.articles
     }
   };
 }
